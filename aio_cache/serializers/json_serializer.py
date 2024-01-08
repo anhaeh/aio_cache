9938a71a14b5f8c6ce6ffe0ea94131a186d5138b
Andres Haehnel <anhaeh@gmail.com>
2024-01-08 16:15:16 -0300

    Update json_serializer.py

--- aio_cache/serializers/json_serializer.py
+++ aio_cache/serializers/json_serializer.py
@@ -7,7 +7,7 @@ class JsonSerializer(Serializer):
 
     @classmethod
     def encode(cls, value: Any) -> bytes:
-        return orjson.dumps(value)
+        return orjson.dumps(value, default=str)
 
     @classmethod
     def decode(cls, value: str) -> Any:
